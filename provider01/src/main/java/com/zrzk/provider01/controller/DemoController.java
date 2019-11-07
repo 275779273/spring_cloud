213b8aa5e2122e57cfafc71015f97f13445d47e5
bie <275779273@qq.com>
2019-11-07 22:57:35 +0800

    2019.11.7 22:57

--- provider01/src/main/java/com/zrzk/provider01/controller/DemoController.java
+++ provider01/src/main/java/com/zrzk/provider01/controller/DemoController.java
@@ -1,15 +1,21 @@
 package com.zrzk.provider01.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/demo1")
 @RestController
+@RefreshScope
 public class DemoController {
+
+    @Value ( "${env}" )
+    private String env;
 
     @RequestMapping("/get")
     public String get(){
-        System.out.println("provider01");
-        return "provider01";
+        System.out.println("provider01   "+env);
+        return "provider01"+env;
     }
 }
